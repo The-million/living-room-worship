3578089649e5c209c9587a5bd2f8de954b83f6ba
The-million <glodinonde@gmail.com>
2026-01-05 04:21:25 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template, request, send_file
+import sqlite3
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        phone = request.form["phone"]
+        email = request.form["email"]
+
+        conn = sqlite3.connect("database.db")
+        c = conn.cursor()
+        c.execute(
+            "INSERT INTO registrations (name, phone, email) VALUES (?, ?, ?)",
+            (name, phone, email)
+        )
+        conn.commit()
+        conn.close()
+
+        return render_template("success.html")
+
+    return render_template("register.html")
+@app.route("/export")
+def export_excel():
+    conn = sqlite3.connect("database.db")
+    df = pd.read_sql_query("SELECT * FROM registrations", conn)
+    conn.close()
+
+    file_name = "inscriptions_event.xlsx"
+    df.to_excel(file_name, index=False)
+
+    return send_file(file_name, as_attachment=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port)
+
+
